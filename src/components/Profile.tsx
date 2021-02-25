e373b9eb66196e094e477bf27a6492ddcf93e4dc
Laércio Almeida <66281702+Laercio2@users.noreply.github.com>
2021-02-25 12:07:39 -0300

    Aula #04 - Context API / Browser sounds and tips

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/Laercio2.png" alt="Laercio Almeida"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Laercio Almeida</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
        
